6d6e5c794213c16b451677f7b05145a34c6039a0
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-02-25 12:40:52 -0300

    [add] - Finaliza estrutura da splashView

--- meusFilmes/Controllers/SplashViewController.swift
+++ meusFilmes/Controllers/SplashViewController.swift
@@ -22,6 +22,28 @@ class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(contentView)
+    }
+    
+    private func setConstraints(){
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
     
 }
--- meusFilmes/Views/SplashView.swift
+++ meusFilmes/Views/SplashView.swift
@@ -11,9 +11,25 @@ class SplashView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        backgroundColor = . red
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        
+    }
+    
+    private func setConstraints(){
+        
     }
 }
